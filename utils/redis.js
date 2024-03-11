8bf99489e818457de7cacd9d60bffc6be843f95a
TeeMeeLehin <samemerald8@gmail.com>
2024-03-11 22:47:57 +0100

    added files

---
+++ utils/redis.js
@@ -0,0 +1,55 @@
+const redis = require('redis');
+
+class RedisClient {
+    constructor() {
+        this.client = redis.createClient();
+
+        this.client.on('error', (err) => {
+            console.error('Redis Error:', err);
+        });
+    }
+
+    isAlive() {
+        return this.client.connected;
+    }
+
+    async get(key) {
+        return new Promise((resolve, reject) => {
+            this.client.get(key, (err, reply) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(reply);
+                }
+            });
+        });
+    }
+
+    async set(key, value, durationInSeconds) {
+        return new Promise((resolve, reject) => {
+            this.client.setex(key, durationInSeconds, value, (err, reply) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(reply);
+                }
+            });
+        });
+    }
+
+    async del(key) {
+        return new Promise((resolve, reject) => {
+            this.client.del(key, (err, reply) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(reply);
+                }
+            });
+        });
+    }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
